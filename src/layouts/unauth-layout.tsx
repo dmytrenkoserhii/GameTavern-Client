f5fabeec17e9db6e68d7a01d0b9d83eb83a042b0
Serhii <dmtrnk.srg@gmail.com>
2024-11-15 17:28:05 +0200

    feat(SCRUM-8): add auth and unauth layouts

---
+++ src/layouts/unauth-layout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Container, Stack } from "@mantine/core";
+import { Footer, UnauthHeader } from "@/components";
+
+export const UnauthLayout: React.FC = () => {
+  return (
+    <Stack h="100dvh" w="100%" justify="space-between">
+      <UnauthHeader />
+
+      <Container
+        component="main"
+        maw="80rem"
+        w="100%"
+        px={0}
+        m="0 auto"
+        flex="1"
+      >
+        <Outlet />
+      </Container>
+
+      <Footer />
+    </Stack>
+  );
+};
