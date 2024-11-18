e06a2cdaf609f96a1f0d1ce0ecc8faf67cfdbb26
Arsen Dmytrenko <arsen.dmitrenko1994@gmail.com>
2024-11-18 11:08:14 +0200

    fix(scrum-31): remove unused const

--- src/constants/color-themes.constant.ts
+++
@@ -1,7 +0,0 @@
-import { SelectItem } from '@/types';
-
-export const COLOR_THEMES: SelectItem[] = [
-  { value: 'light', label: 'Light' },
-  { value: 'dark', label: 'Dark' },
-  { value: 'auto', label: 'System' },
-];
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,2 @@
 export { LANGUAGES } from './languages.constant';
 export { NAVBAR_LINKS } from './navbar-links.constant';
-export { COLOR_THEMES } from './color-themes.constant';
