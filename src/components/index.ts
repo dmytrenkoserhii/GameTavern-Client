12ee6d0e5bbfbd7252ec89e425e708847362b175
Arsen Dmytrenko <arsen.dmitrenko1994@gmail.com>
2024-11-18 10:28:46 +0200

    fix: remove user-icon export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { Header } from './layout/header';
 export { Footer } from './layout/footer';
 export { Navbar } from './layout/navbar';
-export { UserIcon } from './user-icon';
 export { LanguageSelector } from './language-selector';
