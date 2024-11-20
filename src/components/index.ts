4535ea2be7d0e00a7851481cb6485c0b5476fa4a
Serhii <dmtrnk.srg@gmail.com>
2024-11-20 20:47:33 +0200

    feat(scrum-30): add spiner component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Header } from './layout/header';
 export { Footer } from './layout/footer';
 export { Navbar } from './layout/navbar';
 export { LanguageSelector } from './language-selector';
+export { Spinner } from './spinner';
