19f3e26db3ef52e9fea75fbeaea90b49b01f08df
Serhii <dmtrnk.srg@gmail.com>
2024-11-20 15:34:38 +0200

    feat(SCRUM-47): add auth signin and signup services

---
+++ src/features/authentication/services/authentication.service.ts
@@ -0,0 +1,12 @@
+import { publicAxios } from '@/lib';
+
+import { SignInRequestData, SignUpRequestData, User } from '../types';
+
+export const AuthService = {
+  async signUp(signUpData: SignUpRequestData) {
+    return publicAxios.post<{ user: User }>('/auth/sign-up', signUpData);
+  },
+  async signIn(signInData: SignInRequestData) {
+    return publicAxios.post<{ user: User }>('/auth/sign-in', signInData);
+  },
+};
