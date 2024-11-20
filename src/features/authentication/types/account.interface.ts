5f3dd8084ca6d86515c2ae40f41375000300a3a7
Serhii <dmtrnk.srg@gmail.com>
2024-11-20 15:35:30 +0200

    feat(SCRUM-47): add account and user interface

---
+++ src/features/authentication/types/account.interface.ts
@@ -0,0 +1,10 @@
+export interface Account {
+  id: number;
+  username: string;
+  biography: string;
+  birthDate: string;
+  phone: string;
+  avatar: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
---
+++ src/features/authentication/types/user.interface.ts
@@ -0,0 +1,18 @@
+import { Role } from '../enums';
+import { Account } from './account.interface';
+
+export interface User {
+  id: number;
+  email: string;
+  hashedPassword: string;
+  role: Role;
+  isAccountFilled: boolean;
+  refreshToken: string;
+  oauthId: string | null;
+  isEmailVerified: boolean;
+  resetPasswordToken: string | null;
+  resetPasswordTokenExpiresAt: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+  account: Account;
+}
