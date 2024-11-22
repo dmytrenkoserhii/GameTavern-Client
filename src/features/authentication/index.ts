e191924c04d1f027203dc7cf1b7b0f7dd18aab0a
Serhii <dmtrnk.srg@gmail.com>
2024-11-22 12:36:06 +0200

    feat(scrum-29): add logout button functional

--- src/features/authentication/index.ts
+++ src/features/authentication/index.ts
@@ -1,6 +1,3 @@
-export { SignInPage } from './pages';
-export { SignUpPage } from './pages';
-export { ForgotPasswordPage } from './pages';
-export { ResetPasswordPage } from './pages';
-export { EmailConfirmation } from './pages';
-export { EmailVerification } from './pages';
+export type { User } from './types';
+export { AuthService } from './services';
+export { Role } from './enums';
--- src/features/authentication/services/authentication.service.ts
+++ src/features/authentication/services/authentication.service.ts
@@ -1,6 +1,7 @@
+import { User } from '@/features/users';
 import { publicAxios } from '@/lib';
 
-import { SignInRequestData, SignUpRequestData, User } from '../types';
+import { SignInRequestData, SignUpRequestData } from '../types';
 
 export const AuthService = {
   async signUp(signUpData: SignUpRequestData) {
@@ -8,5 +9,8 @@ export const AuthService = {
   },
   async signIn(signInData: SignInRequestData) {
     return publicAxios.post<{ user: User }>('/auth/sign-in', signInData);
+  },
+  async logout() {
+    return publicAxios.get('/auth/logout');
   },
 };
