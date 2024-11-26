b78bc957037cd4cb13d596939f2acc2f435cc3f4
Serhii <dmtrnk.srg@gmail.com>
2024-11-26 20:50:26 +0200

    feat(scrum-61): add game card and item components

---
+++ src/features/lists/components/game-card.tsx
@@ -0,0 +1,27 @@
+import { Card, Stack, Text } from '@mantine/core';
+
+interface GameCardProps {
+  game: {
+    id: number;
+    name: string;
+    description: string;
+    image: {
+      medium_url: string;
+    };
+  };
+  onClick: () => void;
+}
+
+export const GameCard: React.FC<GameCardProps> = ({ game }) => {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      {/* <Image src={game.image.medium_url} alt={game.name} /> */}
+      <Stack gap="xs" mt="md">
+        <Text fw={700} size="lg">
+          {game.name}
+        </Text>
+        <Text size="sm">{game.description}</Text>
+      </Stack>
+    </Card>
+  );
+};
---
+++ src/features/lists/components/game-item.tsx
@@ -0,0 +1,23 @@
+import { Group, Paper, Text } from '@mantine/core';
+
+interface GameItemProps {
+  game: {
+    id: number;
+    name: string;
+    description: string;
+  };
+  onClick: () => void;
+}
+
+export const GameItem: React.FC<GameItemProps> = ({ game, onClick }) => {
+  return (
+    <Paper shadow="xs" p="md" withBorder onClick={onClick} style={{ cursor: 'pointer' }}>
+      <Group>
+        <Text size="md" fw={500}>
+          {game.name}
+        </Text>
+        <Text size="sm">{game.description}</Text>
+      </Group>
+    </Paper>
+  );
+};
