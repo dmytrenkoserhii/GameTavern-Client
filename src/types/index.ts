055cef218c9d2b8147261cb4c77449ecff381b32
Serhii <dmtrnk.srg@gmail.com>
2024-12-02 21:38:46 +0200

    feat(scrum-60): add type for sort options

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export type { SelectItem } from './select-item.interface';
 export type { NavbarLink } from './navbar-link.interface';
 export type { ViewMode } from './view-mode.interface';
+export type { SortOption } from './sort-option.interface';
