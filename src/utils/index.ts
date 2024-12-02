afa87f545d0f93d93df6d88783e03cf26b6bf26f
Serhii <dmtrnk.srg@gmail.com>
2024-12-02 21:35:36 +0200

    feat(scrum-60): add removeObjectEmptyProperties util

---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export { getCurrentQueryParams } from './get-current-query-params';
+export { GenerateReleaseYearOptions } from './generate-release-year-options';
+export { removeObjectEmptyProperties } from './remove-object-empty-properties';
