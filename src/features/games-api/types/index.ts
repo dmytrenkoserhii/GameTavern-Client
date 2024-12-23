684abaa4cdd3085e262a24dcc8ac0907f8d5bcde
Serhii <dmtrnk.srg@gmail.com>
2024-12-23 14:44:23 +0200

    feat(scrum-75): add game api interface

---
+++ src/features/games-api/types/index.ts
@@ -0,0 +1,3 @@
+export type { GameApi } from './game-api.interface';
+export type { GetGamesApiResponceData } from './get-games-api-response-data.interface';
+export type { Platform } from './platform.interface';
