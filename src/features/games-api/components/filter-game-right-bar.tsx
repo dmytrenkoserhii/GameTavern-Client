e6b5e6da4cc48d90b1175c956ed393b0392e25fb
Serhii <dmtrnk.srg@gmail.com>
2024-12-23 14:39:02 +0200

    feat(scrum-75): add filter bar for games page

---
+++ src/features/games-api/components/filter-game-right-bar.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+
+import { useTranslation } from 'react-i18next';
+
+import { Button, Drawer, Select } from '@mantine/core';
+import { useForm } from '@mantine/form';
+
+import { useQuery } from '@tanstack/react-query';
+
+import { ListFilterData, ListQueryParams } from '@/types';
+import { generateYearsOptions } from '@/utils';
+
+import { GamesApiService } from '../services';
+
+interface FilterGameRightBarProps {
+  queryParams: Partial<ListQueryParams>;
+  onFilterChange: (data: Partial<ListFilterData>) => void;
+}
+
+const RELEASE_YEAR_OPTIONS = generateYearsOptions(2010);
+
+export const FilterGameRightBar: React.FC<FilterGameRightBarProps> = ({
+  queryParams,
+  onFilterChange,
+}) => {
+  const { t } = useTranslation();
+
+  const form = useForm<ListFilterData>({
+    initialValues: {
+      releaseYear: '',
+      platform: '',
+    },
+  });
+
+  const prevParams = React.useRef(queryParams);
+
+  const [opened, setOpened] = React.useState(false);
+
+  // React.useEffect(() => {
+  //   if (Object.keys(queryParams).length > 0) {
+  //     form.setValues(queryParams);
+  //   } else {
+  //     form.reset();
+  //   }
+  // }, [queryParams, form]);
+
+  const { data: platforms } = useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => GamesApiService.getAllPlatforms(),
+  });
+
+  React.useEffect(() => {
+    if (JSON.stringify(prevParams.current) !== JSON.stringify(queryParams)) {
+      if (Object.keys(queryParams).length > 0) {
+        form.setValues(queryParams);
+      } else {
+        form.reset();
+      }
+      prevParams.current = queryParams;
+    }
+  }, [queryParams]);
+
+  function onSubmit(data: ListFilterData) {
+    onFilterChange(data);
+    setOpened(false);
+  }
+
+  return (
+    <>
+      <Button onClick={() => setOpened(true)}>{t('games_api.filter_bar.filter_button')}</Button>
+      <Drawer
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title={t('games_api.filter_bar.drawer_title')}
+        padding="xl"
+        size="xl"
+        position="right"
+      >
+        <form onSubmit={form.onSubmit(onSubmit)}>
+          <Select
+            label={t('games_api.filter_bar.release_year.label')}
+            placeholder={t('games_api.filter_bar.release_year.placeholder')}
+            data={RELEASE_YEAR_OPTIONS}
+            {...form.getInputProps('releaseYear')}
+          />
+          <Select
+            label={t('games_api.filter_bar.platform.label')}
+            placeholder={t('games_api.filter_bar.platform.placeholder')}
+            data={
+              platforms?.map((platform) => ({
+                value: platform.id.toString(),
+                label: platform.name,
+              })) ?? []
+            }
+            {...form.getInputProps('platform')}
+            mt="md"
+          />
+          <Button type="submit" mt="md">
+            {t('general.save_changes')}
+          </Button>
+        </form>
+      </Drawer>
+    </>
+  );
+};
