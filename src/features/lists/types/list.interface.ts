543538f1097dd3e982df6973ef103b8e957f313d
Serhii <dmtrnk.srg@gmail.com>
2024-12-23 14:55:56 +0200

    refactor(scrum-75): minor fixes regarding the list page

--- src/features/lists/types/list.interface.ts
+++ src/features/lists/types/list.interface.ts
@@ -1,10 +1,8 @@
-import { Game } from './game.interface';
-
 export interface List {
   id: number;
   name: string;
   description: string;
   createdAt: Date;
   updatedAt: Date;
-  games?: Game[];
+  games?: [];
 }
