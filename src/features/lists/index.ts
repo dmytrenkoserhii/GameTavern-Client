8eafd4e714cf7e5fb7d9e074c3a7cdb600c4cb1e
Serhii <dmtrnk.srg@gmail.com>
2024-12-23 14:48:34 +0200

    chore(scrum-75): temporary exports, later will remove it to game feature

--- src/features/lists/index.ts
+++ src/features/lists/index.ts
@@ -1,1 +1,2 @@
-export type { ListQueryParams, BaseQueryParams, FilterListRightBarData } from './types';
+export { DISPLAY_OPTIONS, SORT_GAMES_OPTIONS } from './constants';
+export { GamesCardView, GamesItemView } from './components';
