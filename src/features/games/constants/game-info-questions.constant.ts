ec714d349bc4ce57022b261ecf5abc5cf6c884ee
Serhii <dmtrnk.srg@gmail.com>
2025-02-01 17:27:06 +0200

    refactor(scrum-87): final fix

--- src/features/games/constants/game-info-questions.constant.ts
+++ src/features/games/constants/game-info-questions.constant.ts
@@ -6,31 +6,31 @@ export const GAME_INFO_QUESTIONS: GameInfoQuestion[] = [
   {
     icon: BsClock,
     translationKey: 'completion_time',
-    question: 'How long does it take to beat',
+    question: 'How long does it take to beat this game?',
   },
   {
     icon: BsJoystick,
     translationKey: 'difficulty',
-    question: 'What is the difficulty level of',
+    question: 'What is the difficulty level of this game?',
   },
   {
     icon: BsBook,
     translationKey: 'story_summary',
-    question: 'Provide a brief spoiler-free summary of',
+    question: 'Provide a brief spoiler-free summary of this game.',
   },
   {
     icon: BsListCheck,
     translationKey: 'pros_cons',
-    question: 'What are the main pros and cons of',
+    question: 'What are the main pros and cons of this game?',
   },
   {
     icon: BsLightbulb,
     translationKey: 'beginner_tips',
-    question: 'What are the most important tips for beginners starting',
+    question: 'What are the most important tips for beginners playing this game?',
   },
   {
     icon: BsCpu,
     translationKey: 'system_requirements',
-    question: 'What are the recommended system requirements to run',
+    question: 'What are the recommended system requirements to play this game?',
   },
 ];
